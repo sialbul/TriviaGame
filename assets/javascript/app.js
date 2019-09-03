473f6cbd5c6ac35ede492e8e3474e7e914ad23f3
Sibel <sbaslamisli@yahoo.com>
2019-09-02 22:49:48 -0400

    timer is working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,74 +1,68 @@
 $(document).ready(function () {
-
     $("#container3").hide();
-
-
+    $("#container4").hide();
     questionList = [
         {
             question: "What stone is the Great Pyramid of Giza made of?",
-            options: ["Limestone", "Marble", "Granite", "Travertine"],
+            option: ["Limestone", "Marble", "Granite", "Travertine"],
             answer: "Limestone",
             image: "assets/images/greatPyramid.jpg"
         },
         {
             question: "What is the length of The Great Wall of China?",
-            options: ["13170 mi", "7500 mi", "17100 mi", "2500 mi"],
+            option: ["13170 mi", "7500 mi", "17100 mi", "2500 mi"],
             answer: "13170 mi",
             image: "assets/images/greatWall.jpg",
         },
         {
             question: "When was the Hagia Sophia built?",
-            options: ["6th century", "10th century", "12nd century", "18th century"],
+            option: ["6th century", "10th century", "12nd century", "18th century"],
             answer: "6th century",
             image: "assets/images/hagiaSophia.jpg",
 
         },
         {
             question: "Where is Machu Picchu?",
-            options: ["Peru", "Mexico", "Indiana", "China"],
+            option: ["Peru", "Mexico", "Indiana", "China"],
             answer: "Peru",
             image: "assets/images/MachuPicchu.jpg",
 
         },
         {
             question: "How many visitors are allowed to visit Potala Palace per day?",
-            options: [2300, 10, 120, 1500],
+            option: [2300, 10, 120, 1500],
             answer: 2300,
             image: "assets/images/potalaPAlace.jpg",
 
         },
         {
             question: "Where is Stonehenge?",
-            options: ["England", "Indiana", "Ireland", "Scotland"],
+            option: ["England", "Indiana", "Ireland", "Scotland"],
             answer: "England",
             image: "assets/images/Stonehenge.jpg",
 
         },
         {
             question: "What stone is the Taj Mahal made of?",
-            options: ["Marble", "Limestone", "Granite", "Travertine"],
+            option: ["Marble", "Limestone", "Granite", "Travertine"],
             answer: "Marble",
             image: "assets/images/Tajmahal.jpg",
 
         },
         {
             question: "How many entrances are there in the Colosseum?",
-            options: [80, 50, 30, 10],
+            option: [80, 50, 30, 10],
             answer: "80",
             image: "assets/images/colosseum.jpg",
 
         }]
-
-
     var question;
-    var options;
+    var option;
     var correctCount = 0;
     var wrongCount = 0;
     var unanswerCount = 0;
     var image;
-    var timer;
     var pick;
-    var timer = 5;
     var intervalId;
     var questionCount = questionList.length;
     var running = false;
@@ -78,121 +72,93 @@ $(document).ready(function () {
     //var losses = 0;
     //var pass = 0;
 
-
-    var i = 0;
-    var currentQuestion;
-    var populateQuestion = function () {
-        currentQuestion = questionList[i]; +
-            console.log(currentQuestion);
-        $("#question").text(currentQuestion.question);
-        $("#option1").text(currentQuestion.options[0]);
-        $("#option2").text(currentQuestion.options[1]);
-        $("#option3").text(currentQuestion.options[2]);
-        $("#option4").text(currentQuestion.options[3]);
-        $("#questionImg").attr("src", currentQuestion.image);
-        i++;
-        runTimer();
-    };
-
-    populateQuestion();
-
     $("#start").on("click", function () {
         $("#container2").hide();
         $("#container3").show();
         $("#container4").hide();
-
-    });
-
-
-
-    $(".option").on("click", function () {
-
         populateQuestion();
-
+        runTimer();
     });
 
     function runTimer() {
         if (!running) {
-            timer = 5
+            timer=5;
+            clearInterval(intervalId);
             intervalId = setInterval(decrement, 1000);
-            running = true;
+            // $("#timeleft").show();
+            // $("#answerblock").hide();
+        }
+        //  The decrement function.
+        function decrement() {
+            timer--;
+            $("#timeleft").html("<h5>Time remaining:   " + timer + "</h5>");
+            //stop timer if reach 0
+            if (timer == 0) {
+                // stop();
+                clearInterval(intervalId);
+                displayedCorrect(); 
+                populateQuestion();
+            }
         }
     }
+    var i = 0;
+    var currentQuestion;
 
-    //  The decrement function.
-    function decrement() {
+    var populateQuestion = function () {
+        runTimer();
+        currentQuestion = questionList[i]; +
+            console.log(currentQuestion);
+        $("#question").text(currentQuestion.question);
+        $("#option1").text(currentQuestion.option[0]);
+        $("#option2").text(currentQuestion.option[1]);
+        $("#option3").text(currentQuestion.option[2]);
+        $("#option4").text(currentQuestion.option[3]);
+        $("#questionImg").attr("src", currentQuestion.image);
+        i++;
+    };
 
-        $("#timeleft").html("<h3>Time remaining:   " + timer + "</h3>");
-        timer--;
-
-        //stop timer if reach 0
-        if (timer === 0) {
-            unanswerCount++;
-            stop();
-            $("#answerblock").html("<h3>" + "Correct answer:   " + currentQuestion.answer + "</h3>");
-            //1000;
-            setTimeout(populateQuestion, 4000);
-
-        }
+    function displayedCorrect() {
+        $("#answerblock").html("<h3>" + "Correct answer:   " + currentQuestion.answer + "</h3>");
     }
+});
+// //  The stop function
+function stop() {
+    clearInterval(intervalId);
+    //  Clears our intervalId
+    //  We just pass the name of the interval
+    //  to the clearInterval function.
+}
+// //  Execute the run function.
 
-
-    //  The stop function
-    function stop() {
-        running = false;
-
-        //  Clears our intervalId
-        //  We just pass the name of the interval
-        //  to the clearInterval function.
-        clearInterval(intervalId);
+$("#option").on("click", function () {
+    populateQuestion();
+    for (var i = 0; i < questionList.length; i++) {
+        holder.push(questionList[i]);
     }
+    //grab array position from userGuess
+    userGuess = parseInt($(this).attr("data-guessvalue"));
 
-    //  Execute the run function.
-    runTimer();
+    //correct guess or wrong guess outcomes
+    if (userGuess === this.answer) {
+        stop();
+        correctCount++;
+        userGuess = "";
+        $("#answerblock").html("<p>Correct!</p>");
+        setTimeout(2000);
 
-
-
-    $("#container3").html("<h2>" + pick.question + "</h2>");
-    for (var i = 0; i < pick.options.length; i++) {
-        var userChoice = $("<div>");
-        userChoice.addClass("answerchoice");
-        userChoice.html(pick.options[i]);
-        //assign array position to it so can check answer
-        userChoice.attr("data-guessvalue", i);
-        $("#answerblock").append(userChoice);
-        //		}
+    } else {
+        stop();
+        wrongCount++;
+        userGuess = "";
+        $("#answerblock").html("<p>Wrong! The correct answer is: " + currentQuestion.answer + "</p>");
+        setTimeout(4000);
     }
+})
 
+//run the score screen if all questions answered
+console.log("wrongCount = " + wrongCount + ", correctCount = " + correctCount);
 
-
-    //click function to select answer and outcomes
-    $(".answerchoice").on("click", function () {
-        //grab array position from userGuess
-        userGuess = parseInt($(this).attr("data-guessvalue"));
-
-        //correct guess or wrong guess outcomes
-        if (userGuess === pick.answer) {
-            stop();
-            correctCount++;
-            userGuess = "";
-            $("#answerblok").html("<p>Correct!</p>");
-            setTimeout(4000);
-
-        } else {
-            stop();
-            wrongCount++;
-            userGuess = "";
-            $("#answerblock").html("<p>Wrong! The correct answer is: " + currentQuestion.answer + "</p>");
-            setTimeout(4000);
-        }
-
-    })
-
-
-    //run the score screen if all questions answered
-    console.log("wrongCount = " + wrongCount + ", correctCount = " + correctCount);
-
-
+function resultPage() {
     if ((wrongCount + correctCount + unanswerCount) === questionCount.length) {
         $("#container4").show();
         $("#container3").hide();
@@ -210,15 +176,14 @@ $(document).ready(function () {
         populateQuestion();
 
     }
-
-
-
-    $("#restart").on("click", function () {
-        for (var i = 0; i < holder.length; i++) {
-            options.push(holder[i]);
-        }
-        runTimer();
-        populateQuestion();
-    })
 }
-)
+resultPage();
+
+$("#restart").on("click", function () {
+    for (var i = 0; i < holder.length; i++) {
+        options.push(holder[i]);
+    }
+    runTimer();
+    populateQuestion();
+})
+
